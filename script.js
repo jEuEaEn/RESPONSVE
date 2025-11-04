db34d4201d43ef85db30c0ea6b862c892abc3376
jEuEaEn <jarm1981avrm@gmail.com>
2025-11-04 18:46:33 -0500

    Por fin mrd

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 const boton = document.getElementById("buton")
+const min = document.getElementById("men")
 
+boton.addEventListener("click", () =>{
+
+
+    const aH = document.createElement("a")
+    aH.textContent = "Home"
+    min.appendChild(aH)
+    
+    const aB = document.createElement("a")
+    aB.textContent = "About"
+    min.appendChild(aB)
+    
+    const Bg = document.createElement("button")
+    Bg.textContent = "Blog"
+    Bg.id = "Bloh"
+    min.appendChild(Bg)
+    
+    
+    min.className = "Menu"
+    
+    console.log("BBB")
+
+    const BlogP = document.getElementById("Bloh")
+    
+    BlogP.addEventListener("click", () => {
+    
+        min.className = "MenuN"
+        min.classList.remove("Menu")
+
+        aH.remove()
+        aB.remove()
+        Bg.remove()
+    
+    })
+
+})
+
+/*
 boton.addEventListener("click", () => {
 
     if(!document.querySelector(".Menu")){
@@ -8,20 +46,10 @@ boton.addEventListener("click", () => {
         document.body.appendChild(menu)
 
         
-        const aH = document.createElement("a")
-        menu.appendChild(aH)
-        
-        const aB = document.createElement("a")
-        menu.appendChild(aB)
-        
-        const Bg = document.createElement("a")
-        menu.appendChild(Bg)
-        
-        menu.className = "Menu"
-        
-        console.log("BBB")
 
 
+        
     }
 
 })
+*/
